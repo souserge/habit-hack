fb5eaf3ac8c608b199f4ba5c8484e959a2a808db
Taangh <30960806+Taangh@users.noreply.github.com>
2017-11-23 11:20:05 +0100

    User dbo models

--- HabitHack/main/models.py
+++ HabitHack/main/models.py
@@ -3,11 +3,20 @@ from django.db import models
 # Create your models here.
 
 # This is our first model
-class User(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    # ...
 
-    # TODO:
-    #   specify all fields and their constraints,
-    #   add __str__ method
+
+class User(models.Model):
+    username = models.OneToOneField(Credentials, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=40)
+    email = models.CharField(max_length=50)
+    city = models.CharField(max_length=20)
+    country = models.CharField(max_length=20)
+    age = models.IntegerField()
+    gender_choice = (('MALE', 'M'), ('FEMALE', 'F'))
+    gender = models.CharField(max_length=1, choices=gender_choice)
+
+
+class Credentials(models.Model):
+    username = models.CharField(max_length=20, primary_key=True, unique=True)
+    password = models.CharField(max_length=20)
+
