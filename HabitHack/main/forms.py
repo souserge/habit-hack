0a93c98812672f3cbed7f984ff7422c5598015a3
Serge Korzh <serge.korzh@gmail.com>
2017-11-24 11:25:45 +0100

    Add .bat for running server

---
+++ HabitHack/main/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+# test form, delete it afterwards
+class ContactForm(forms.Form):
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
