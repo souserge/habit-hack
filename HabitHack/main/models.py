3951837903abecbe6ee5fd574db3d67159ddca39
Serge Korzh <serge.korzh@gmail.com>
2017-11-25 13:53:28 +0100

    Fix 'Credentials not found' error in Models

--- HabitHack/main/models.py
+++ HabitHack/main/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-
-# This is our first model
-
+class Credentials(models.Model):
+    username = models.CharField(max_length=20, primary_key=True, unique=True)
+    password = models.CharField(max_length=20)
 
 class User(models.Model):
     username = models.OneToOneField(Credentials, on_delete=models.CASCADE, primary_key=True)
@@ -14,9 +14,4 @@ class User(models.Model):
     age = models.IntegerField()
     gender_choice = (('MALE', 'M'), ('FEMALE', 'F'))
     gender = models.CharField(max_length=1, choices=gender_choice)
-
-
-class Credentials(models.Model):
-    username = models.CharField(max_length=20, primary_key=True, unique=True)
-    password = models.CharField(max_length=20)
 
