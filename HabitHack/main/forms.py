ffdda9c80bce6fc3ff8f3c69f3648c35f24c4787
alex <alex@example.com>
2017-12-06 23:12:31 +0100

    add SignUpForm in forms.py

--- HabitHack/main/forms.py
+++ HabitHack/main/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm 
 from django.contrib.auth.models import User
 
 
@@ -9,3 +10,12 @@ class ContactForm(forms.Form):
     sender = forms.EmailField()
     cc_myself = forms.BooleanField(required=False)
 
+# User sign up form
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
