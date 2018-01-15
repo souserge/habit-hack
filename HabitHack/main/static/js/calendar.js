bbfb677e959d20a764cb2974574d140dfab0a4ea
Serge Korzh <serge.korzh@gmail.com>
2018-01-16 00:16:35 +0100

    Complete basic calendar frontend functionality

--- HabitHack/main/static/js/calendar.js
+++ HabitHack/main/static/js/calendar.js
@@ -1,19 +1,25 @@
 // Define classes
 class Habit {
-    constructor(name, weekdays, numRepeats) {
+    constructor(id, name, weekdays, numRepeats) {
+        this.id = id;
         this.name = name;
         this.weekdays = weekdays;
         this.numRepeats = numRepeats > 0 ? numRepeats : 1;
         this.history = new Map();
     }
 
-    incrementRecord(dateHash) {
+    incrementRecord(date) {
+        let dateHash = dateToHash(date);
         let rec = this.history.get(dateHash);
-        if (!rec && this.isActiveWeekday(hashToDate(dateHash).getDay())) {
-            this.history.set(new Record());
+        if (!rec) {
+            if (this.isActiveWeekday(date.getDay())) {
+                rec = new Record();
+                this.history.set(dateHash, rec);
+            }
         } else if (rec.isLowerThan(this.numRepeats)) {
             rec.incr();
         }
+        return rec;
     }
 
     decrementRecord(date) {
@@ -21,6 +27,7 @@ class Habit {
         if (rec && rec.isHigherThan(0)) {
             rec.decr();
         }
+        return rec;
     }
 
     isActiveWeekday(weekday) {
@@ -71,61 +78,98 @@ function hashToDate(hash) {
     return new Date(hash);
 }
 
+function getDate(dayOffset) {
+    let date = new Date();
+    date.setDate(date.getDate() - dayOffset);
+    return date;
+}
 
 
 
 
 // Define sample data
-let pushups  = new Habit("Pushups", new Set([0, 1, 2, 3, 4, 5, 6]), 30);
-let guitar   = new Habit("Guitar", new Set([0, 4, 5, 2]), 3);
-let swimming = new Habit("Swimming", new Set([1, 3, 6]), 1);
+let pushups  = new Habit(0, "Pushups", new Set([0, 1, 2, 3, 4, 5, 6]), 30);
+let guitar   = new Habit(1, "Guitar", new Set([0, 4, 5, 2]), 3);
+let swimming = new Habit(2, "Swimming", new Set([1, 3, 6]), 1);
 habits = [ pushups, guitar, swimming ];
 
 $(() => {
     alert('It works!');
     let $table = $('#habits-table');
+    createHeader($table);
+
     for (let habit of habits) {
-        let appendStr = '<tr class="habit-row">';
-        appendStr += '<td class="habit-cell-name">' + habit.name + '</td>';
+        createHabitRow(habit, $table)
+    }
 
-        for (let i = 0; i < 7; i++) {
-            appendStr += '<td class="habit-cell">';
-            appendStr += habit.isActiveWeekday(i) ? createDayCell(habit.numRepeats) : createDayCell(0);
-            appendStr += '</td>';
-        }
+   $('.habit-cell-btn_active').on('click', function(ev) {
+        let dayOffset = parseInt($(this).closest('.habit-cell').attr('value'));
+        let habitId = parseInt($(this).closest('.habit-row').attr('value'));
+        let habit = habits.find((h) => h.id == habitId);
+        console.log("Offset: " + dayOffset + "; habitId: " + habitId);
+        let date = getDate(dayOffset);
+        let rec = habit.incrementRecord(date);
 
-        appendStr += '</tr>';
-        $table.append(appendStr);
-   }
-
-   $('.habit-cell-btn').on('click', () => {
-        // percent += 100/times;
-        // drawSector(percent);
+        drawSector(rec.counter / habit.numRepeats, $(this));
    });
 });
 
 
-function createDayCell(goalNum) {
-    return '<div class="habit-cell-btn">'
-            + '<div class="habit-cell-btn-circle">'
-            + '<span class="habit-cell-btn-day">' + goalNum + '</span>'
-            + '</div>'
-            + '</div>';
+function createHeader($table) {
+    let appendStr = '<tr class="days-row">';
+    
+
+    for (let i = 6; i >= 0; i--) {
+        let d = getDate(i);
+        appendStr += '<th class="day-cell">';
+        appendStr += d.toDateString();
+        appendStr += '</th>';
+    }
+    appendStr += '</tr>';
+    $table.append(appendStr);
 }
 
-function drawSector(prec) {
-    let activeBorder = $("#activeBorder");
+function createHabitRow(habit, $table) {
+    let appendStr = '<tr class="habit-row" value="' + habit.id + '">';
+
+    for (let i = 6; i >= 0; i--) {
+        let dow = getDate(i).getDay();
+
+        appendStr += '<td class="habit-cell" value="' + i + '">';
+        appendStr += habit.isActiveWeekday(dow) ? createDayCell(habit.numRepeats) : createDayCell(0);
+        appendStr += '</td>';
+    }
+
+    appendStr += '<td class="habit-cell-name">' + habit.name + '</td>';
+
+    appendStr += '</tr>';
+    $table.append(appendStr);
+}
+
+
+function createDayCell(goalNum) {
+    return goalNum > 0 
+            ?   '<div class="habit-cell-btn habit-cell-btn_active">'
+                + '<div class="habit-cell-btn-circle habit-cell-btn-circle_active">'
+                + '<span class="habit-cell-btn-day">' + goalNum + '</span>'
+                + '</div></div>'
+            :   '<div class="habit-cell-btn habit-cell-btn_inactive">'
+                + '<div class="habit-cell-btn-circle habit-cell-btn-circle_inactive">'
+                + '</div></div>';
+}
+
+function drawSector(prec, $border) {
     if (prec >= 1)
         prec = 1;
     let deg = prec*360;
     if (deg <= 180){
-        activeBorder.css('background-image','linear-gradient(' + (90+deg) + 'deg, transparent 50%, #A2ECFB 50%),linear-gradient(90deg, #A2ECFB 50%, transparent 50%)');
+        $border.css('background-image','linear-gradient(' + (90+deg) + 'deg, transparent 50%, #A2ECFB 50%),linear-gradient(90deg, #A2ECFB 50%, transparent 50%)');
     }
     else {
-        activeBorder.css('background-image','linear-gradient(' + (deg-90) + 'deg, transparent 50%, #39B4CC 50%),linear-gradient(90deg, #A2ECFB 50%, transparent 50%)');
+        $border.css('background-image','linear-gradient(' + (deg-90) + 'deg, transparent 50%, #39B4CC 50%),linear-gradient(90deg, #A2ECFB 50%, transparent 50%)');
     }
     
     let startDeg = 90;
-    activeBorder.css('transform','rotate(' + startDeg + 'deg)');
-    $("#circle").css('transform','rotate(' + (-startDeg) + 'deg)');
+    $border.css('transform','rotate(' + startDeg + 'deg)');
+    $border.find(".habit-cell-btn-circle").first().css('transform','rotate(' + (-startDeg) + 'deg)');
 }
