64fa40ac4883fa355aea5f53235655adb03aef40
Serge Korzh <serge.korzh@gmail.com>
2018-01-17 10:51:15 +0100

    Update home view to pass habits to template

--- HabitHack/main/home_views.py
+++ HabitHack/main/home_views.py
@@ -7,7 +7,29 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_http_methods
 
+from .models import UserHabit, HabitHistory
+
+
 
 @login_required
 def home(request):
-    return render(request, 'main/home.html')
+    user = request.user
+    user_habits = UserHabit.objects.filter(user_id=user.id)
+
+    habits = []
+
+    for habit in user_habits:
+        habit_records = HabitHistory.objects.filter(user_habit_id=habit.id)
+        records = {}
+        for rec in habit_records:
+            records.add(rec.datehash, rec.counter)
+
+        habits.add({ 
+            'id': habit.id,
+            'name': habit.name,
+            'weekdays': habit.weekdays,
+            'num_repeats': habit.numRepeats,
+            'history': records
+        })
+
+    return render(request, 'main/home.html', { 'habits': habits })
