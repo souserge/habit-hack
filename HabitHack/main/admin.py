e0708f96b6d157a669a7319b65d6fdc7de1571ef
Serge Korzh <serge.korzh@gmail.com>
2018-01-17 11:22:09 +0100

    Register UserHabit and HabitHistory

--- HabitHack/main/admin.py
+++ HabitHack/main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, UserHabit, HabitHistory
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(UserHabit)
+admin.site.register(HabitHistory)
